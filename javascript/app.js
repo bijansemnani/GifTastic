3105db00b251af5407b659d8b83b47f29ee8ea39
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-27 12:18:46 -0800

    added play pause functionality when user clicks on the gif and cleaned up the code a bit

--- javascript/app.js
+++ javascript/app.js
@@ -1,30 +1,61 @@
 $(document).ready(function() {
   var topics = ["cars","djs","videogames","fortnite","overwatch"];
   renderButtons();
+
   function renderButtons() {
         $("#topicButton").empty();
         for (var i = 0; i < topics.length; i++) {
           $("#topicButton").append("<button class='topic'>" + topics[i] + "</button>");
         }
+  }
 
+  function splitValue(value, index) {
+    console.log(value.substring(0, index));
+    console.log(value.substring(index));
+    return [value.substring(0, index),value.substring(index)];
   }
 
   $("#topicButton").on("click","button.topic", function () {
     var topic = $(this).text();
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q="+
-    topic+"&api_key=yDCNh74O3PEtUiFjweP41yuQjaffRREA&limit=5";
+    topic+"&api_key=yDCNh74O3PEtUiFjweP41yuQjaffRREA&limit=10";
     $.ajax({
       url: queryUrl,
       method: "GET"
     }).then(function (data) {
-
+      $("#dump").empty();
       for (var i = 0; i < data.data.length; i++) {
-        console.log(data.data[i].images.original.url);
         var imageUrl = data.data[i].images.original.url;
-        $("#dump").append("<img src='"+imageUrl+"'>");
+        var split = splitValue(imageUrl, imageUrl.length-4);
+        split[1]="_s.gif";
+        split = split.join('');
+        $("#dump").append("<img data-state='still' id='gif' class='gifs' src='"+split+"'>");
       }
+    });
+  });
 
-    })
+  $("#dump").on("click","img.gifs", function () {
+    console.log("here");
+    var state = $(this).attr("data-state");
+    var url = $(this).attr("src");
+    if(state === "still"){
+      $(this).attr("data-state","animate");
+      var split = splitValue(url, url.length-6);
+      console.log(split);
+      split[1] = ".gif";
+      url = split.join('');
+      console.log(url);
+      $(this).attr("src",url);
+    }
+    else{
+      $(this).attr("data-state","still");
+      var split = splitValue(url, url.length-4);
+      console.log(split);
+      split[1] = "_s.gif";
+      url = split.join('');
+      console.log(url);
+      $(this).attr("src",url);
+    }
   });
 
 });
