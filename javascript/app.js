361e4a23445043c5200e901d15d8a932affd5ab1
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-27 12:25:38 -0800

    added comments

--- javascript/app.js
+++ javascript/app.js
@@ -1,59 +1,80 @@
 $(document).ready(function() {
   var topics = ["cars","djs","videogames","fortnite","overwatch"];
+
   renderButtons();
 
+  //initial function to render the given buttons
   function renderButtons() {
-        $("#topicButton").empty();
-        for (var i = 0; i < topics.length; i++) {
-          $("#topicButton").append("<button class='topic'>" + topics[i] + "</button>");
-        }
+    $("#topicButton").empty();
+    for (var i = 0; i < topics.length; i++) {
+      $("#topicButton").append("<button class='topic'>"
+      + topics[i] + "</button>");
+    }
   }
 
   function splitValue(value, index) {
-    console.log(value.substring(0, index));
-    console.log(value.substring(index));
     return [value.substring(0, index),value.substring(index)];
   }
 
   $("#topicButton").on("click","button.topic", function () {
+    //get the topic name from the button then set up the queryUrl
     var topic = $(this).text();
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q="+
     topic+"&api_key=yDCNh74O3PEtUiFjweP41yuQjaffRREA&limit=10";
+
+    //make the ajax call to get json info
     $.ajax({
       url: queryUrl,
       method: "GET"
     }).then(function (data) {
       $("#dump").empty();
+
+      //traverse the json info and print the images
       for (var i = 0; i < data.data.length; i++) {
+        //get imageUrl then split it to the url and the extension
         var imageUrl = data.data[i].images.original.url;
         var split = splitValue(imageUrl, imageUrl.length-4);
+
+        //replace the extension with _s.gif then join the url
         split[1]="_s.gif";
         split = split.join('');
+
+        //display the images on the page
         $("#dump").append("<img data-state='still' id='gif' class='gifs' src='"+split+"'>");
       }
     });
   });
 
+  //when user clicks on gif it either pauses or plays
   $("#dump").on("click","img.gifs", function () {
-    console.log("here");
+    //get the state and url from the button click
     var state = $(this).attr("data-state");
     var url = $(this).attr("src");
+
+    //if the state is still play the gif
     if(state === "still"){
+      //set the data-state to be animate
       $(this).attr("data-state","animate");
+
+      //split the url and change the extension to .gif then join
       var split = splitValue(url, url.length-6);
-      console.log(split);
       split[1] = ".gif";
       url = split.join('');
-      console.log(url);
+
+      //set the img src to the new url
       $(this).attr("src",url);
     }
+    //if the state is animate then pause the gif
     else{
+      //set the data-state to be still
       $(this).attr("data-state","still");
+
+      //split the url and change the extension to _s.gif then join
       var split = splitValue(url, url.length-4);
-      console.log(split);
       split[1] = "_s.gif";
       url = split.join('');
-      console.log(url);
+
+      //set the img src to the new url
       $(this).attr("src",url);
     }
   });
